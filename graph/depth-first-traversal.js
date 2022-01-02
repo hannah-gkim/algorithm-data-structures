f8a1d5a629f6e96c378cc7d7419a6022928919b5
hannahk0108 <hannahk0108@gmail.com>
2022-01-01 18:11:48 -0600

    depthfirst

---
+++ graph/depth-first-traversal.js
@@ -0,0 +1,40 @@
+//dept first uss stack
+//iterative approach
+// const depthFirstPrint = (graph, source) => {
+//   const stack = [source]; //
+//   const depthfirst = [];
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+//     depthfirst.push(current);
+//     for (let neighbor of graph[current]) {
+//       stack.push(neighbor);
+//     }
+//   }
+//   console.log(depthfirst);
+//   return depthfirst;
+// };
+
+//recursive
+const depthFirstPrint = (graph, source, depthfirst = []) => {
+  //   let depthfirst = [];
+  depthfirst.push(source);
+  for (let neighbor of graph[source]) {
+    //when [] don't make recursive calls
+    depthFirstPrint(graph, neighbor, depthfirst);
+  }
+  return depthfirst;
+};
+
+const graph = {
+  a: ["c", "b"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+};
+
+depthFirstPrint(graph, "a"); //abdfce or //acebdf
+console.log(depthFirstPrint(graph, "a"));
+
+//breadtg first uses queue
